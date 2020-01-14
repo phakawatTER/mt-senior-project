4b0e91666e667d07acc644731b24d763d2a43859
phakawat <phakawat@phakawats-MacBook-Pro-2255.local>
2020-01-14 14:43:56 +0700

    better node alignment

--- graph.py
+++ graph.py
@@ -41,14 +41,16 @@ SIZE = plt.rcParams["figure.figsize"]
 SIZE[0] = 15
 SIZE[1] = 10
 IS_FIRST = True
+SCHOOL_NODE_SIZE = 1000
+NORMAL_NODE_SIZE = 300
 
 class Render:
-    global MAJOR
+    global MAJOR,SCHOOL_NODE_SIZE,NORMAL_NODE_SIZE,SCHOOL
     def __init__(self):
         
-        self.tmpSubject = []
+        self.subjects = []
         self.NODE_COORDINATES = {}
-        
+        self.NODE_SIZES = []
         # BACK BUTTON CALLBACK FUNCTION
         def back(*args, **kwargs):
             global HISTORY_INDEX
@@ -81,22 +83,19 @@ class Render:
             bottom = 0.0 , # the bottom of the subplots of the figure
             top = 0.94    , # the top of the subplots of the figure
         )
-        self.Draw()
+        self.draw()
         fig = plt.figure(1) # REFERENCE TO CURRENT PLOT
         fig.set_facecolor("#000000") # SET BACKGROUND COLOR
         # SET CALLBACK FUNCTION FOR PLOT ON CLICK
         fig.canvas.mpl_connect('button_press_event', lambda e:self.removeNodeOnClick(event=e,
-                coords=self.NODE_COORDINATES,subjects=self.tmpSubject)) 
-        ### SHOW PLOT
-        plt.show()
+                coords=self.NODE_COORDINATES,subjects=self.subjects)) 
+        plt.show() # SHOT PLOT
+    ### END CLASS INIT ###
         
-    ### CLASS INIT ###
-        
-    
     def rerender(self):
         plt.clf()
         plt.cla()
-        self.Draw()
+        self.draw()
         plt.pause(0.0001)
         
     # CALLBACK FUNCTION FOR GRAPH ON CLICK
@@ -104,7 +103,11 @@ class Render:
         global HISTORY_INDEX
         clickx = event.xdata # X COORDINATE
         clicky = event.ydata # Y COORDINATE
+        print(f"{clickx},{clicky}")
         targetName = ""
+        matched = []
+        minDistance = None
+        targetName = None
         # CALCULATE EUCLIDEAN DISTANCE 
         for subject in coords:
             try:
@@ -114,13 +117,20 @@ class Render:
                 distance = math.pow(math.pow((x-clickx), 2) +
                                     math.pow((y-clicky), 2), 0.5)
                 if distance < 0.25:
-                    targetName = subject
+                    matched.append((subject,distance))
+                
             except Exception:
                 pass
-                # print(err)
-                
+            
+        for (s,d) in matched:
+            if not minDistance or d < minDistance:
+                targetName = s
+                minDistance = d
+            
+                        
         # IF EUCLIDEAN DISTANCE MATCH WITH SOME NODE SO DELETE THAT NODE
         if targetName != "":
+            print(f"remove {targetName}")
             removed = [
                 course for course in subjects if course["subject"] != targetName]
             removed = copy.deepcopy(removed)
@@ -130,27 +140,34 @@ class Render:
         ######***** END ON CLICK CALLBACK FUNCTION *****######
         
     
-        
-    def Draw(self):
+    # Draw everything
+    def draw(self):
         G = nx.DiGraph()
-        self.tmpSubject = HISTORY[HISTORY_INDEX] 
+        self.subjects = HISTORY[HISTORY_INDEX] 
         self.NODE_COORDINATES = {}
+        self.NODE_SIZES = []
         rootNodes = []
         childNodes = []
         ROOT_LEVEL = 0
         COLOR_MAP = []
         ROOT_NODE_COLORS = []
-
+        PREV_LEVEL_Y = 0
+        SCHOOL_LABEL = ""
+        for index,s in enumerate(SCHOOL.split(" "),start=0):
+            SCHOOL_LABEL += f"{s}"
+            if index < len(SCHOOL.split(" "))-1:
+                SCHOOL_LABEL += "\n"
         # FILTER ROOT NODE
-        for node in copy.deepcopy(self.tmpSubject):
+        for node in copy.deepcopy(self.subjects):
             if "prerequisite" not in node:
                 rootNodes.append(node)
         LEVELS = len(rootNodes)  # total root nodes (length of array)
-        G.add_node(MAJOR, pos=(-2, -int(LEVELS/2)))  # ADD SCHOOL NODE
+        G.add_node(SCHOOL_LABEL, pos=(-2, -int(LEVELS/2)))  # ADD SCHOOL NODE
+        self.NODE_SIZES.append(SCHOOL_NODE_SIZE)
         COLOR_MAP.append(backgrounds[len(backgrounds)-1]) # APPEND LAST COLOR OF THE LIST TO BE COLOR OF ROOT NODE
 
         # FILTER CHILD NODE ** NODE THAT HAS PREREQUISITE
-        for node in copy.deepcopy(self.tmpSubject):
+        for node in copy.deepcopy(self.subjects):
             if "prerequisite" in node:
                 childNodes.append(node)
 
@@ -164,15 +181,18 @@ class Render:
             COLOR_MAP.append(NODE_COLOR)
             ROOT_NODE_COLORS.append({"root":subject,"color":NODE_COLOR})
             # ADD ROOT NODE TO GRAPH
-            G.add_node(subject, pos=(1+math.pow(-1,abs(ROOT_LEVEL))*0.07, ROOT_LEVEL))
+            G.add_node(subject, pos=(1+math.pow(-1,abs(ROOT_LEVEL))*0.07, PREV_LEVEL_Y))
             # ADD EDGE FROM ROOT NODE TO SCHOOL NODE
-            G.add_edge(MAJOR, subject, weight=5,root=MAJOR)
+            G.add_edge(SCHOOL_LABEL, subject, weight=5,root=SCHOOL_LABEL)
             PARENT_CURRENT_Y = ROOT_LEVEL
-            PARENT_CURRENT_X = 1+math.pow(-1,abs(ROOT_LEVEL))*0.07
+            PARENT_CURRENT_Y = PREV_LEVEL_Y
+            PARENT_CURRENT_X = 1 + math.pow(-1,abs(ROOT_LEVEL))*0.07
             self.NODE_COORDINATES[subject] = (PARENT_CURRENT_X, PARENT_CURRENT_Y)
-            Parents = [subject]
+            self.NODE_SIZES.append(NORMAL_NODE_SIZE)
+            PARENTS = [subject]
+            Y_COORDINATES = []
             while True:
-                parent = Parents.pop(0)  # POP FIRST ELEMENT OF PARENTS
+                parent = PARENTS.pop(0)  # POP FIRST ELEMENT OF PARENTS
                 offsetY = 0.5  # Y COORDINATE OFFSET
                 index = 0  # INDEX
                 for child in childNodes:
@@ -187,23 +207,31 @@ class Render:
                             CURRENT_X = PARENT_COORDINATES[0] + 2+math.pow(-1,abs(ROOT_LEVEL))*0.2 # X COORDINATE
                             CURRENT_Y = PARENT_COORDINATES[1] + \
                                 (offsetY-1*index)  # Y COORDINATE
+                            Y_COORDINATES.append(CURRENT_Y)
                             if _subject not in self.NODE_COORDINATES:
                                 COLOR_MAP.append(NODE_COLOR)
                             self.NODE_COORDINATES[_subject] = (CURRENT_X, CURRENT_Y)
+                            self.NODE_SIZES.append(NORMAL_NODE_SIZE)
                             # ADD NODE TO GRAPH
                             G.add_node(_subject, pos=(CURRENT_X, CURRENT_Y))
                             # ADD EDGE FROM PARENT TO CHILD NODE
                             G.add_edge(_prereq, _subject, weight=3,root=subject)
-                            Parents.append(_subject)  # PUSH NEW PARENT TO THE LIST
+                            PARENTS.append(_subject)  # PUSH NEW PARENT TO THE LIST
                             index += 1
+                
                 # IF THERE'S NO ELEMENTS IN PARENTS LIST ; BREAK THE LOOP
-                if len(Parents) == 0:
+                if len(PARENTS) == 0:
                     break
-            # UPDATE LEVEL OF ROOT NODE
+            #### END WHILE LOOP ####
+
             ROOT_LEVEL -= LEVEL_SUBTRACTION
-       
+            # UPDATE LEVEL OF ROOT NODE
+            if len(Y_COORDINATES) == 0:
+                PREV_LEVEL_Y = PREV_LEVEL_Y - LEVEL_SUBTRACTION # UPDATE PREVIOUS Y COORDINATE
+            else:
+                minY = min(Y_COORDINATES)
+                PREV_LEVEL_Y = minY - LEVEL_SUBTRACTION
             
-        # SET LAYOUT
         pos = nx.get_node_attributes(G, 'pos')
         while(len(G)<len(COLOR_MAP)):
             COLOR_MAP.pop()
@@ -213,6 +241,7 @@ class Render:
             G,
             pos,
             node_color=COLOR_MAP,
+            node_size=self.NODE_SIZES,
             alpha=0.5,
             linewidths=2
         )
@@ -222,6 +251,7 @@ class Render:
         
         child_edges = {}
         for (u,v,d) in G.edges(data=True):
+            
             root = d["root"]
             if root == MAJOR:
                 continue
@@ -258,6 +288,7 @@ class Render:
         # nx.draw_networkx_edges(G, pos, edgelist=credit2_edges, width=2,edge_color="#FFFFFF",alpha=1.00)
         # # DRAW 1 CREDIT EDGES
         # nx.draw_networkx_edges(G, pos, edgelist=credit1_edges, width=1,edge_color="#ffffff",alpha=1.00)
+        
         plt.axis("off") # TURN OFF AXIS
         title_obj = plt.title(f"School of {SCHOOL}") # SET TITLE
         plt.gcf().canvas.set_window_title(f"School of {SCHOOL}") # SET WINDOW TITLE
