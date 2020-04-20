714c929bd73b78515c86602b592ea07b9bfa8150
phakawat <phakawat@phakawat-p.local>
2020-04-20 15:14:20 +0700

    higher graph resolution

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-pyinstaller control.spec
+rm -rf dist # remove prev version #dist
+rm -rf build # remove prev version of build
+pyinstaller control.spec # freeze the code
+python TCLChanger.py # change TCL version
